96a5e292e6cc9ad2137302c6a0e92d6613ded548
kacpersmaga <kacper.smaga@microsoft.wsei.edu.pl>
2024-07-12 19:10:55 +0200

    feat: make operators work

--- script.js
+++ script.js
@@ -18,3 +18,24 @@ console.log(add(5,5));
 console.log(subtract(5,5));
 console.log(multiply(5,5));
 console.log(divide(5,5));
+
+let num1;
+let num2;
+let operator;
+
+function operate(num1, num2, operator){
+    switch(operator){
+        case '+':
+            add(num1, num2);
+            break;
+        case '-':
+            subtract(num1, num2);
+            break;
+        case '*':
+            multiply(num1, num2);
+            break;
+        case '/':
+            divide(num1, num2);
+            break;
+    }
+}
